d121d34a38261cb83d8a6f49560aba0d924a6370
HeleneH <helene.houplain@epitech.eu>
2026-01-03 12:56:48 +0100

    feat(play): add `Number` struct for button handling and implement play state display and interactions

---
+++ src/play_state.rs
@@ -0,0 +1,67 @@
+use piston_window::{rectangle, text, Context, G2d, Glyphs, MouseButton, Transformed};
+use crate::app_state::AppState;
+use crate::button::ButtonRect;
+use crate::display::CELL_SIZE;
+use crate::grid::Grid;
+
+pub struct Number {
+   vector: Vec<ButtonRect>
+}
+
+impl Number {
+    pub fn new() -> Self {
+        Self {
+            vector: vec![]
+        }
+    }
+
+    pub fn fill_vector(&mut self) {
+        let mut start_x = 460.0 + 50.0;
+        let mut start_y = 115.0;
+        for i in 0..9 {
+            self.vector.push( ButtonRect::flat(start_x, start_y, 45.0, 38.0, &*(i+1).to_string(), [0.61, 0.30, 0.8, 1.0], [0.87, 0.66, 1.0, 1.0]));
+            start_x += 50.0;
+            if i == 2 || i == 5 {
+                start_y += 50.0;
+                start_x = 460.0 + 50.0;
+            }
+        }
+    }
+}
+
+// pub fn init_play_state() {
+//
+// }
+
+pub fn press_button_play(
+    numbers: &Number,
+    mouse: [f64; 2],
+    app_state: &mut AppState,
+) {
+    let x = app_state.selected_cell().unwrap().0;
+    let y = app_state.selected_cell().unwrap().1;
+
+    if app_state.get_grid().original[y][x] {
+        return;
+    }
+    print!("{} - {} is hovered\n", x, y);
+    for (i, button) in numbers.vector.iter().enumerate() {
+        if button.is_hovered(mouse) {
+            let value = (i + 1) as u8;
+            app_state
+                .grid_mut()
+                .add_to_grid(y, x, value as u32);
+        }
+    }
+}
+
+pub fn display_play(vector: &Number,
+                    app_state: &AppState,
+                    c: &Context,
+                    g: &mut G2d,
+                    glyphs: &mut Glyphs
+) {
+    for i in 0..9 {
+        vector.vector[i].draw(c, g, glyphs, vector.vector[i].is_hovered(app_state.get_mousse_pos()));
+    }
+}
