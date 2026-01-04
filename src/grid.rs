80f8faefcc6dcae4071c93a9a98a0ac1bd85ec3d
HeleneH <helene.houplain@epitech.eu>
2026-01-04 17:23:11 +0100

    feat(core): add `get_grid_ori` method to retrieve original cell states

--- src/grid.rs
+++ src/grid.rs
@@ -16,6 +16,9 @@ impl Grid {
     pub fn get_grid(&self) -> [[u32 ; 9]; 9] {
         self.grid
     }
+    pub fn get_grid_ori(&self) -> [[bool ; 9]; 9] {
+        self.original
+    }
 
     pub fn add_to_grid(&mut self, x: usize, y: usize, value: u32) {
         // TO-DO check if it in the size of the grid
