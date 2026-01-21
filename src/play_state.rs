00663bd5482ffd9eb668326752c9b0192269b48c
HeleneH <helene.houplain@epitech.eu>
2026-01-21 09:55:17 +0100

    refactor(play): encapsulate play state logic into `Play` struct with methods for interaction and display

--- src/play_state.rs
+++ src/play_state.rs
@@ -1,7 +1,7 @@
-use piston_window::{image, rectangle, Context, G2d, G2dTexture, Glyphs, Transformed};
+use piston_window::{image, rectangle, Context, Flip, G2d, G2dTexture, Glyphs, PistonWindow, Texture, TextureSettings, Transformed};
 use crate::app_state::AppState;
 use crate::button::ButtonRect;
-use crate::display::{read_file_play, State, BTN_BG, BTN_HOVER};
+use crate::display::{read_file_play, State, BTN_BG, BTN_HOVER, WINDOW_H, WINDOW_W};
 use crate::solver;
 
 const MAX_SUDOKU: usize = 5;
@@ -31,97 +31,130 @@ impl Number {
     }
 }
 
-pub fn press_number_button(
-    numbers: &Number,
-    mouse: [f64; 2],
-    app_state: &mut AppState,
-    life: &mut u32
-) {
-    let x = app_state.selected_cell().unwrap().0;
-    let y = app_state.selected_cell().unwrap().1;
 
-    if app_state.get_grid().original[y][x] {
-        return;
-    }
+pub struct Play {
+    new_sudoku: ButtonRect,
+    back: ButtonRect,
+    life: u32,
+    numbers: Number,
+    texture_lives: G2dTexture,
+}
 
-    let mut grid = app_state.grid_mut().clone();
-
-    solver::is_valid(&mut grid, 0);
-
-    for (i, button) in numbers.vector.iter().enumerate() {
-        if button.is_hovered(mouse) {
-            let value = (i + 1) as u8;
-
-            if value as u32 != grid.grid[y][x] {
-                *life -= 1;
-                continue;
-            }
-            app_state
-                .grid_mut()
-                .add_to_grid(y, x, value as u32);
+impl Play {
+    pub fn new(window: &mut PistonWindow) -> Self {
+        Self {
+            new_sudoku: ButtonRect::flat(WINDOW_W / 1.3, WINDOW_H / 15.0, 130.0, 38.0, "New sudoku", [0.61, 0.30, 0.8, 1.0], [0.87, 0.66, 1.0, 1.0]),
+            back: ButtonRect::flat(WINDOW_W / 1.3, WINDOW_H / 15.0, 130.0, 38.0, " < ", [0.61, 0.30, 0.8, 1.0], [0.87, 0.66, 1.0, 1.0]),
+            life: 3,
+            numbers: Number::new(),
+            texture_lives: Texture::from_path(&mut window.create_texture_context(), "assets/images/life.png", Flip::None, &TextureSettings::new(), ).expect("Download failed : life"),
         }
     }
-}
 
-pub fn press_button_play (
-    mouse: [f64; 2],
-    new_sudoku: &ButtonRect,
-    app_state: &mut AppState,
-    life: &mut u32
-) {
-    if new_sudoku.is_hovered(mouse) {
-        parse_file(new_sudoku, app_state);
-        *life = 3;
+    pub fn get_life(&self) -> u32 {
+        self.life
     }
-}
+    pub fn set_life(&mut self, new_life: u32) {
+        self.life = new_life
+    }
 
-pub fn check_remain_life(life: u32, state: &mut State) {
-    if life < 1 {
-        *state = State::Lost;
+    pub fn init_number(&mut self) {
+        self.numbers.fill_vector();
     }
-}
 
-pub fn parse_file(new_sudoku: &ButtonRect,
-              app_state: &mut AppState) {
-    let file_name = format!("sudoku_not_resolved/sudoku_{}.txt", app_state.sudoku_counter());
-    if let Ok(grid) = read_file_play(file_name) {
-        app_state.set_grid(grid);
+    fn parse_file(&mut self,
+                  app_state: &mut AppState) {
+        let file_name = format!("sudoku_not_resolved/sudoku_{}.txt", app_state.sudoku_counter());
+        if let Ok(grid) = read_file_play(file_name) {
+            app_state.set_grid(grid);
+        }
+        if app_state.sudoku_counter() >= MAX_SUDOKU {
+            app_state.set_sudoku_counter(0);
+        }
+        app_state.set_sudoku_counter(app_state.sudoku_counter() + 1);
     }
-    if app_state.sudoku_counter() >= MAX_SUDOKU {
-        app_state.set_sudoku_counter(0);
-    }
-    app_state.set_sudoku_counter(app_state.sudoku_counter() + 1);
-}
 
-pub fn display_play(vector: &Number,
-                    app_state: &mut AppState,
-                    c: &Context,
-                    g: &mut G2d,
-                    glyphs: &mut Glyphs,
-                    new_sudoku: &ButtonRect,
-                    texture_life: &G2dTexture,
-                    life: u32
-) {
-    if !app_state.grid_mut().original[0].contains(&true) {
-        parse_file(&new_sudoku, app_state);
+    pub fn press_button_play(&mut self,
+                              mouse: [f64; 2],
+                              app_state: &mut AppState,
+    ) {
+        if let Some((x, y)) = crate::display::get_cell_from_mouse(mouse) {
+            if !app_state.get_grid().get_grid_ori()[y][x] {
+                app_state.set_selected_cell(x, y);
+            }
+        }
+        if app_state.selected_cell().is_some() {
+            self.press_number_button(mouse, app_state);
+        }
+        if self.new_sudoku.is_hovered(mouse) {
+            self.parse_file(app_state);
+            self.set_life(3);
+        }
     }
-    rectangle(
-        [0.90, 0.92, 0.97, 1.0],
-        [480.0, 140.0, 250.0, 240.0],
-        c.transform,
-        g,
-    );
-    for i in 0..9 {
-        vector.vector[i].draw(c, g, glyphs, vector.vector[i].is_hovered(app_state.get_mousse_pos()), 20);
-    }
-    new_sudoku.draw(c, g, glyphs, new_sudoku.is_hovered(app_state.get_mousse_pos()), 18);
 
-    let mut x = 50.0;
-    for i in 0..life {
-        let transform = c.transform
-            .trans(x, 85.0)
-            .scale(0.05, 0.05);
-        image(texture_life, transform, g);
-        x += 20.0;
+    pub fn check_remain_life(&self, state: &mut State) {
+        if self.life < 1 {
+            *state = State::Lost;
+        }
+    }
+
+    pub fn display_play(&mut self,
+                        app_state: &mut AppState,
+                        c: &Context,
+                        g: &mut G2d,
+                        glyphs: &mut Glyphs,
+    ) {
+        if !app_state.grid_mut().original[0].contains(&true) {
+            self.parse_file(app_state);
+        }
+        rectangle(
+            [0.90, 0.92, 0.97, 1.0],
+            [480.0, 140.0, 250.0, 240.0],
+            c.transform,
+            g,
+        );
+        for i in 0..9 {
+            self.numbers.vector[i].draw(c, g, glyphs, self.numbers.vector[i].is_hovered(app_state.get_mousse_pos()), 20);
+        }
+        self.new_sudoku.draw(c, g, glyphs, self.new_sudoku.is_hovered(app_state.get_mousse_pos()), 18);
+
+        let mut x = 50.0;
+        for i in 0..self.life {
+            let transform = c.transform
+                .trans(x, 85.0)
+                .scale(0.05, 0.05);
+            image(&self.texture_lives, transform, g);
+            x += 20.0;
+        }
+    }
+
+    pub fn press_number_button(&mut self,
+        mouse: [f64; 2],
+        app_state: &mut AppState,
+    ) {
+        let x = app_state.selected_cell().unwrap().0;
+        let y = app_state.selected_cell().unwrap().1;
+
+        if app_state.get_grid().original[y][x] {
+            return;
+        }
+
+        let mut grid = app_state.grid_mut().clone();
+
+        solver::is_valid(&mut grid, 0);
+
+        for (i, button) in self.numbers.vector.iter().enumerate() {
+            if button.is_hovered(mouse) {
+                let value = (i + 1) as u8;
+
+                if value as u32 != grid.grid[y][x] {
+                    self.life -= 1;
+                    continue;
+                }
+                app_state
+                    .grid_mut()
+                    .add_to_grid(y, x, value as u32);
+            }
+        }
     }
 }
