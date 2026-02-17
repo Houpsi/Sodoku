dd93e6add2495bc5d14ca56cfe6b0258ccb2b6c3
HeleneH <helene.houplain@epitech.eu>
2026-02-17 11:28:50 +0100

    refactor(win): improve leaderboard rendering and remove debug print

--- src/win_state.rs
+++ src/win_state.rs
@@ -21,7 +21,7 @@ impl Win {
             retry : ButtonRect::flat((WINDOW_W / 2.0) + 80.0, (WINDOW_H / 2.0) + 10.0, 70.0, 38.0, "Retry", BTN_HOVER, [0.87, 0.66, 1.0, 1.0]),
             menu: ButtonRect::flat(WINDOW_W / 3.2, (WINDOW_H / 2.0) + 10.0, 70.0, 38.0, "Menu", BTN_HOVER, [0.87, 0.66, 1.0, 1.0]),
             quit: ButtonRect::flat((WINDOW_W / 2.0) - 30.0, WINDOW_H / 1.47, 70.0, 38.0, "Quit", BTN_HOVER, [0.87, 0.66, 1.0, 1.0]),
-            texture_win: Texture::from_path(&mut window.create_texture_context(), "assets/images/win_state.png",  Flip::None, &TextureSettings::new(),).expect(" Download failed : game-over."),
+            texture_win: Texture::from_path(&mut window.create_texture_context(), "assets/images/you_win.png",  Flip::None, &TextureSettings::new(),).expect(" Download failed : you-win."),
             leader_board: Self::parse_leader_board().unwrap_or_else(|_| Vec::new())
         }
     }
@@ -63,7 +63,6 @@ impl Win {
             };
             leader_board.push((name, score));
         }
-        println!("hello\n{:?}", leader_board);
         Ok(leader_board)
     }
 
@@ -74,24 +73,38 @@ impl Win {
                               glyphs: &mut Glyphs,
     ) {
         let transform = c.transform
-            .trans((WINDOW_W / 2.0) - 320.0, WINDOW_H / 20.0);
+            .trans((WINDOW_W / 2.0) - 226.5, WINDOW_H / 5.0);
         image(&self.texture_win, transform, g);
         self.retry.draw(c, g, glyphs, self.retry.is_hovered(app_state.get_mousse_pos()), 18);
         self.menu.draw(c, g, glyphs, self.menu.is_hovered(app_state.get_mousse_pos()), 18);
         self.quit.draw(c, g, glyphs, self.quit.is_hovered(app_state.get_mousse_pos()), 18);
-        let leaderboard_text =  self.leader_board.iter().fold(String::new(), |mut acc, (name, score)| {
-            acc.push_str(&format!("{} - {}", name, score));
-            acc
-        });
+        self.display_leader_board(c, g, glyphs);
+    }
 
-        text::Text::new_color([0.15, 0.15, 0.2, 1.0], 15)
-            .draw(
-                &leaderboard_text,
-                glyphs,
-                &c.draw_state,
-                c.transform.trans((WINDOW_W / 2.0), WINDOW_H / 4.0),
-                g,
-            )
-            .unwrap();
+    fn display_leader_board(&self,
+                            c: &Context,
+                            g: &mut G2d,
+                            glyphs: &mut Glyphs
+    ) {
+        let start_y = WINDOW_H / 2.5;
+
+        for (i, (name, score)) in self.leader_board.iter().take(10).enumerate() {
+            let color = if i == 0 {
+                [1.0, 0.8, 0.2, 1.0]
+            } else {
+                [0.15, 0.15, 0.2, 1.0]
+            };
+            let line = format!("{}. {} - {}", i + 1, name, score);
+
+            text::Text::new_color(color, 15)
+                .draw(
+                    &line,
+                    glyphs,
+                    &c.draw_state,
+                    c.transform.trans((WINDOW_W / 1.35), start_y + i as f64 * 20.0),
+                    g,
+                )
+                .unwrap();
+        }
     }
 }
